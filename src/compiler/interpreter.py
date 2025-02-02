a4412cad50e92fb916d65fb51a3d00d7b3436b98
Tuuli Toivanen-Gripentrog <tuuli.k.toivanen@gmail.com>
2025-02-02 10:24:37 +0200

    Variable declaration and scope works in interpreter

--- src/compiler/interpreter.py
+++ src/compiler/interpreter.py
@@ -1,29 +1,73 @@
+from dataclasses import dataclass
 from typing import Any
 from compiler import ast
 
 type Value = int | bool | None
 
+built_ins = {
 
-def interpret(node: ast.Expression) -> Value:
+}
+
+
+@dataclass
+class SymTab():
+    locals: dict
+
+
+@dataclass
+class SymTabChild(SymTab):
+    parent: SymTab | None
+
+
+def interpret(node: ast.Expression, symTab: SymTab) -> Value:
+    def find_variable(variable_name: str, symTab: SymTabChild) -> Value:
+        if variable_name in symTab.locals.keys():
+            return symTab.locals[variable_name]
+        elif isinstance(symTab.parent, SymTabChild):
+            return find_variable(variable_name=variable_name, symTab=symTab.parent)
+        else:
+            raise Exception(f"Unkown variable '{variable_name}'")
+
     match node:
         case ast.BlockExpression():
-            return interpret(node.expressions[0])
+            index = 0
+            sym_tab_child = SymTabChild(parent=symTab, locals=dict())
+            while index < len(node.expressions)-1:
+                interpret(node.expressions[index], sym_tab_child)
+                index = index + 1
+            return interpret(node.expressions[-1], sym_tab_child)
 
         case ast.Literal():
             return node.value
 
         case ast.BinaryOp():
-            a: Any = interpret(node.left)
-            b: Any = interpret(node.right)
+            a: Any = interpret(node.left, symTab)
+            b: Any = interpret(node.right, symTab)
             if node.op.token == '+':
                 return a + b
+            elif node.op.token == '-':
+                return a - b
             elif node.op.token == '<':
                 return a < b
+            elif node.op.token == '*':
+                return a * b
             else:
-                raise ...
+                raise Exception(f"Unsupported operator {node.op.token}")
 
         case ast.IfExpression():
             if interpret(node.condition_branch):
                 return interpret(node.then_branch)
             else:
                 return interpret(node.else_branch)
+
+        case ast.Identifier():
+            return find_variable(node.name, symTab)
+
+        case ast.VariableDeclaration():
+            variable_name = node.variable_name
+            initializer = interpret(node.initializer, symTab)
+            symTab.locals[variable_name] = initializer
+            return None
+
+        case _:
+            raise Exception(f"Unsupported AST node {node}")
--- tests/interpreter_test.py
+++ tests/interpreter_test.py
@@ -1,13 +1,56 @@
+from dataclasses import dataclass
 from compiler.domain import Token, SourceLocation, L, TokenType
 from compiler.ast import BinaryOp, BlockExpression, BooleanLiteral, Expression, FunctionExpression, IfExpression, Literal, Identifier, Operator, UnaryExpression, VariableDeclaration, WhileLoop
 from compiler.parser import parse, parse
 import pytest
 from compiler.tokenizer import tokenize
-from compiler.interpreter import interpret
+from compiler.interpreter import SymTab, interpret
 
 
-def test_interpret_basic() -> None:
-    tokens = tokenize("1 + 2")
-    ast = parse(tokens)
-    result = interpret(ast)
-    assert result == 3
+def test_interpret_basic_arithmetics() -> None:
+    assert tokenize_parse_and_interpret("1 + 2") == 3
+    assert tokenize_parse_and_interpret("3-2") == 1
+    assert tokenize_parse_and_interpret("2*3") == 6
+    assert tokenize_parse_and_interpret("1+2*4") == 9
+
+
+def test_interpret_block() -> None:
+    res = tokenize_parse_and_interpret("1+2; {2+2; 2-1}")
+    assert res == 1
+
+
+def test_parse_block_which_ends_in_semicolon() -> None:
+    res = tokenize_parse_and_interpret("1+2; {2+2; 2-1;}")
+    assert res == None
+
+
+def test_varible_declaration() -> None:
+    res = tokenize_parse_and_interpret("var x = 1; var y = 2; x + y")
+    assert res == 3
+
+
+def test_varible_declaration_in_different_blocks() -> None:
+    res = tokenize_parse_and_interpret(
+        "var x = 1; var y = 2;  var z = {var x = 3; var y = 5; x + y}; x + y + z"
+    )
+    assert res == 11
+
+
+def test_varible_declaration_in_different_blocks_fetches_variable_from_outer_scope() -> None:
+    res = tokenize_parse_and_interpret(
+        "var x = 1; var y = 2;  var z = {x + y}; x + y + z"
+    )
+    assert res == 6
+
+
+def test_varible_declaration_in_different_blocks_fails_if_variable_is_not_found() -> None:
+    with pytest.raises(Exception) as e:
+        tokenize_parse_and_interpret(
+            "var x = 1; var y = 2;  var z = {x + a}; x + y + z"
+        )
+    assert "Unkown variable 'a'" in str(
+        e.value)
+
+
+def tokenize_parse_and_interpret(code: str):
+    return interpret(parse(tokenize(code)), symTab=SymTab(locals=dict()))
